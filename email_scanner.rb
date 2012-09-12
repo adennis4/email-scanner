be6628ed21d883e09915f05e84f41d47f6c39f3f
Andrew Dennis <adennis@groupon.com>
2012-09-12 16:49:35 -0500

    handles multiple name/email pairs on the same line

--- email_scanner.rb
+++ email_scanner.rb
@@ -1,12 +1,11 @@
-class EmailScanner
+
   def scan_for_email(text)
-    email_regex = /("(.*)"\s.\b(mailto:)*)*([\w._%+-]+@[\w.-]+\.[a-z]{2,4})\b./i
+    email_regex = /(?:"([^@]*)"\s.\b(?:mailto:)*)*([\w._%+-]+@[a-zA-Z.-]+\.[a-z]{2,4})\b./i
     text.scan(email_regex)
   end
 
   def get_name_and_email(text)
     scan_for_email(text).map do |person|
-      { :name => person[1], :email => person.last }
+      { :name => person.first, :email => person.last }
     end
   end
-end
